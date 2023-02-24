35687ddffea22ea23f8d6a975e376eed609f1d0f
Denis Shpigun <denish.den@mail.ru>
2023-02-24 10:35:43 +0300

    added first seminar

---
+++ Seminar1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Task 1.
+/*
+Console.Write("Input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number2*number2 == number1) 
+    Console.WriteLine("Yes");
+
+else 
+    Console.WriteLine("No");
+*/
+
+//Task 2. Введите N. Выведите числа от -N до N
+/*
+Console.Write("Input a positive number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = -N;
+while(current<=N)
+{
+    Console.Write(current + " ");
+    current+=1;
+}
+// for(int i = -N; i<N+1; i++){
+//     Console.WriteLine(i);
+// }
+*/
