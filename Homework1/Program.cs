847be53e76161d7cfcc5341b9e8ad26cb4b29e3b
Denis Shpigun <denish.den@mail.ru>
2023-02-24 11:21:57 +0300

    Changed task1 in homework 1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,8 +4,14 @@ Console.Write("Input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1 > num2) Console.WriteLine($"max = {num1}");
-else Console.WriteLine($"max = {num2}");
+if(num1 > num2) 
+{
+Console.WriteLine($"max = {num1}, min = {num2}");
+}
+else 
+{
+Console.WriteLine($"max = {num2}, min = {num1}");
+}
 */
 
 // Task 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
