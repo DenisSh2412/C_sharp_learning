8793ff2711ebdd0067fc2f009cbdc93d02a0e700
Denis Shpigun <denish.den@mail.ru>
2023-03-07 15:19:58 +0300

    Added Seminar4 and Homework4

---
+++ Homework4/Program.cs
@@ -0,0 +1,60 @@
+﻿// Task 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int Pow(int a, int b){
+    int i = 0;
+    int res = 1;
+    while(i<b){
+        res*=a;
+        i+=1;
+    }
+    return res;
+}
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Result is {Pow(num1, num2)} ");
+*/
+
+// Task 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+/*
+int SumOfDigits(int a){;
+    int sum = 0;
+    int new_a = Math.Abs(a);
+    while(new_a>0){
+        sum+=new_a%10;
+        new_a/=10;
+    }
+    return sum;
+}
+
+Console.Write("Input your number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits is {SumOfDigits(num1)} ");
+*/
+
+// Task 3. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+/*
+int[] MakeArray(string s){;
+    string[] new_s = s.Split(", ");
+    int[] num = new int[new_s.Length];
+    for(int i = 0; i<new_s.Length; i++){
+        num[i] = Convert.ToInt32(new_s[i]);
+    }
+    return num;
+}
+
+void PrintArray(int[] arr){;
+    Console.Write("[");
+    for(int i = 0; i<arr.Length-1; i++){
+        Console.Write(arr[i]+", ");
+    }
+    Console.Write(arr[arr.Length-1]+"]");
+}
+
+Console.Write("Input numbers: ");
+string line = Console.ReadLine();
+int[] arr = MakeArray(line);
+PrintArray(arr);
+*/
---
+++ Seminar4/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел
+/*
+int Count(int num){
+    int sum = 0;
+    for(int i = 1; i<=num; i++){sum+=i;}
+    return sum;
+}
+
+Console.Write("Input a number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum is {Count(A)}");
+*/
+
+//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*
+int CountNum(int num){
+    int count = 0;
+    while(num>0){
+        count+=1;
+        num/=10;
+    }
+    return count;
+}
+
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Number of digits: {CountNum(n)}");
+*/
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N
+/*
+int Mul(int num){
+    int mul = 1;
+    for(int i = 1; i<=num; i++){mul*=i;}
+    return mul;
+}
+
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Result is {Mul(n)}");
+*/
+//Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+/*
+void MakeArr(int[] arr){
+    for(int i = 0;i<arr.Length; i++){
+        arr[i] = new Random().Next(0, 2);
+    }
+    for(int i = 0;i<arr.Length; i++){
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] mass = new int[8];
+MakeArr(mass);
+*/
