2935dcab79d13d4d0f35958ccc343d8064f41b21
Denis Shpigun <denish.den@mail.ru>
2023-03-18 14:22:34 +0300

    Added seminar7 and homework7

---
+++ Homework7/Program.cs
@@ -0,0 +1,142 @@
+﻿// Task 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+/*
+void FillArray(double[,] arr){
+    Random rand = new Random();
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            arr[i, j] = Math.Round(rand.Next(-50, 50) + rand.NextDouble(), 2);
+        }
+    }
+}
+
+void PrintArray(double[,] arr){
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            Console.Write(arr[i, j]+ "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[n, m];
+FillArray(array);
+PrintArray(array);
+
+*/
+//Task 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет
+/*
+void IndexorNumber(int[,] arr){
+    Console.WriteLine("Введите команду: 1-поиск по индексу, 2-поиск по элементу");
+    string s = Console.ReadLine();
+    if(s=="1"){
+        Console.Write("Введите строку для элемента: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите столбец для элемента: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        CheckIndex(n, m, arr);
+    }
+    else if(s=="2"){
+        Console.Write("Введите число для поиска: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        CheckNumber(n, arr);
+    }
+    else{
+        Console.Write("Вы ввели неправильную команду");
+    }
+}
+
+int[,] MakeArray(){
+    Random rand = new Random();
+    int[,] arr = new int[rand.Next(2, 10), rand.Next(2, 10)];
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            arr[i, j] = rand.Next(-10, 10); 
+            }
+    }
+    return arr;
+}
+void PrintArray(int[,] arr){
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            Console.Write(arr[i, j]+ "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CheckNumber(int n, int[,] arr){
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            if(arr[i,j]==n){
+                Console.WriteLine($"Индексы числа: [{i}, {j}]");
+                return ;
+            } 
+        }
+    }
+    Console.WriteLine("Такого числа в массиве нет");
+}
+
+void CheckIndex(int n, int m, int[,] arr){
+    if(n<arr.GetLength(0) && m<arr.GetLength(1) && n>=0 && m>=0){
+        Console.WriteLine($"{arr[n, m]}");
+    }
+    else{
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+}
+
+Console.WriteLine("Случайный массив: ");
+int[,] newarr = MakeArray();
+PrintArray(newarr);
+IndexorNumber(newarr);
+*/
+
+
+//Task 3. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+/*
+int[,] MakeArray(int n, int m){
+    Random rand = new Random();
+    int[,] arr = new int[n, m];
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            arr[i, j] = rand.Next(-10, 10); 
+            }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr){
+    for(int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            Console.Write(arr[i, j]+ "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CountAverage(int[,] arr){
+    Console.WriteLine("Средние арифметические по столбцам: ");
+    for(int i = 0; i<arr.GetLength(1); i++){
+        double aver = 0;
+        for(int j = 0; j<arr.GetLength(0); j++){
+            aver+=arr[j, i];
+        }
+        Console.Write($"{Math.Round(aver/arr.GetLength(0), 2)}" + "\t");
+    }
+
+}
+
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] array = MakeArray(n, m);
+PrintArray(array);
+CountAverage(array);
+*/
---
+++ Seminar7/Program.cs
@@ -0,0 +1,246 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+*/
+
+// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i+j;
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+*/
+// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void MatrixPows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if((i%2)==0 && j%2==0) array[i,j] = array[i,j]*array[i,j];
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+MatrixPows(matrix);
+PrintMatrix(matrix);
+*/
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов,
+//  находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void SumOfDiagonalMatrix(int[,] array)
+{
+    int sum =0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i==j) sum = sum+array[i,j];
+        }
+    }
+System.Console.WriteLine($"Сумма диагонали равна: {sum}");
+}
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+SumOfDiagonalMatrix(matrix);
+*/
+
+// Доп. задача
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+int[] FindMin(int[,] arr){
+    int[] min = {0, 0};
+    int minim = arr[0, 0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(arr[i, j]<minim){
+                minim = arr[i, j];
+                min[0]=i;
+                min[1]=j;
+            }
+        }
+    }
+    return min;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintNewArr(int[,] arr, int[] min){
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(i!=min[0] && j!=min[1]){
+                Console.Write(arr[i, j] + "\t");
+            }
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+int[] min = FindMin(matrix);
+Console.WriteLine();
+PrintNewArr(matrix, min);
+*/
